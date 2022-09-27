44d217405b9f27038e42eecd4ba46543fc14db7c
keshav pareek <kkeshavpareekk@gmail.com>
2022-09-28 00:06:29 +0530

    inital commit - project completed

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  allUsers,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.route("/").post(registerUser).get(protect, allUsers);
+router.post("/login", loginUser);
+
+module.exports = router;
