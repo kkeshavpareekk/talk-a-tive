8e6d5d2f1b94a753bfdc1bad191aff7f58c86983
keshav pareek <kkeshavpareekk@gmail.com>
2022-10-10 14:42:26 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -14,13 +15,26 @@ const app = express();
 
 app.use(express.json()); //to accept json data
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+//-------deployment--------
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+//-------deployment---------
 
 app.use(notFound);
 app.use(errorHandler);
